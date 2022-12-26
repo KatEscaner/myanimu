136a0d078aa9a6d1ab811b6e0ac52c0c1abc5ab8
KatEscaner <jjmateo1279@gmail.com>
2022-12-26 19:06:51 +0100

    Create project

---
+++ src/main/java/com/myanimu/MyanimuApplication.java
@@ -0,0 +1,13 @@
+package com.myanimu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyanimuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyanimuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myanimu/MyanimuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myanimu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyanimuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
