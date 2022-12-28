221b79c98cdca7d0eb5b87e2d5d74941bfb10d91
KatEscaner <jjmateo1279@gmail.com>
2022-12-28 19:42:28 +0100

    Create Multimedia Controller

---
+++ src/main/java/com/myanimu/controllers/MultimediaController.java
@@ -0,0 +1,52 @@
+package com.myanimu.controllers;
+
+import com.myanimu.dao.MultimediaDAO;
+import com.myanimu.models.Film;
+import com.myanimu.models.Multimedia;
+import com.myanimu.models.Serie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MultimediaController {
+
+    @Autowired
+    private MultimediaDAO multimediaDAO;
+
+    @RequestMapping(value = "api/multimedias", method = RequestMethod.GET)
+    public List<Multimedia> getMultimedias(){
+        return multimediaDAO.getMultimedias();
+    }
+
+    @RequestMapping(value = "api/series", method = RequestMethod.GET)
+    public List<Serie> getSeries(){
+        return multimediaDAO.getSeries();
+    }
+
+    @RequestMapping(value = "api/films", method = RequestMethod.GET)
+    public List<Film> getFilms(){
+        return multimediaDAO.getFilms();
+    }
+
+    @RequestMapping(value = "api/serie/{id}", method = RequestMethod.GET)
+    public Serie getSerie(@PathVariable int id){
+        return multimediaDAO.getSerie(id);
+    }
+
+    @RequestMapping(value = "api/film/{id}", method = RequestMethod.GET)
+    public Film getFilm(@PathVariable int id){
+        return multimediaDAO.getFilm(id);
+    }
+
+    @RequestMapping(value = "api/serie", method = RequestMethod.POST)
+    public void addSerie(@RequestBody Serie serie){
+        multimediaDAO.addSerie(serie);
+    }
+
+    @RequestMapping(value = "api/film", method = RequestMethod.POST)
+    public void addFilm(@RequestBody Film film){
+        multimediaDAO.addFilm(film);
+    }
+}
