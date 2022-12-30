f40d1da1984f793590b90ff9dcfc83042aa23cbf
KatEscaner <jjmateo1279@gmail.com>
2022-12-30 14:22:19 +0100

    Create json request classes

---
+++ src/main/java/com/myanimu/jsonRequest/ListFilm.java
@@ -0,0 +1,10 @@
+package com.myanimu.jsonRequest;
+
+import lombok.Data;
+
+@Data
+public class ListFilm {
+
+    private int list;
+    private int film;
+}
---
+++ src/main/java/com/myanimu/jsonRequest/ListSerie.java
@@ -0,0 +1,9 @@
+package com.myanimu.jsonRequest;
+
+import lombok.Data;
+
+@Data
+public class ListSerie {
+    private int list;
+    private int serie;
+}
