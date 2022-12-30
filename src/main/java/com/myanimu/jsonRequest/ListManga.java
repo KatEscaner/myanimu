6ae33bdec5ec5da1ce69a663e78022cc6f3e6af0
KatEscaner <jjmateo1279@gmail.com>
2022-12-30 15:53:09 +0100

    Create json request for manga and novel

---
+++ src/main/java/com/myanimu/jsonRequest/ListManga.java
@@ -0,0 +1,10 @@
+package com.myanimu.jsonRequest;
+
+import lombok.Data;
+
+@Data
+public class ListManga {
+
+    private int list;
+    private int manga;
+}
---
+++ src/main/java/com/myanimu/jsonRequest/ListNovel.java
@@ -0,0 +1,10 @@
+package com.myanimu.jsonRequest;
+
+import lombok.Data;
+
+@Data
+public class ListNovel {
+
+    private int list;
+    private int novel;
+}
