8bd8eaac593f389bd598dc7e4173f8244c1a68a8
KatEscaner <jjmateo1279@gmail.com>
2022-12-30 15:04:39 +0100

    Optimice

--- src/main/java/com/myanimu/dao/ListAnimeImpl.java
+++ src/main/java/com/myanimu/dao/ListAnimeImpl.java
@@ -11,7 +11,6 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
